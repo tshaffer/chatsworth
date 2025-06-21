7e39f99c74bdfdc6dd8a0fcb5fbf128a75bcc753
Ted Shaffer <shaffer.family@gmail.com>
2025-06-21 07:50:47 -0700

    initial inclusion of markdown-it

--- backend/src/controllers/markdownImporter.ts
+++ backend/src/controllers/markdownImporter.ts
@@ -1,13 +1,13 @@
 
 import { Request, Response } from 'express';
-import { FileToImport } from '../types';
 import path from 'path';
 import multer from 'multer';
 import * as fs from 'fs';
+import markdownit from 'markdown-it';
 
 export const markdownImporterEndpoint = async (request: Request, response: Response, next: any) => {
 
-    const storage = multer.diskStorage({
+  const storage = multer.diskStorage({
     destination: (req, file, cb) => {
       const pathToUploads = path.join(__dirname, '../../public/uploads');
       console.log('pathToUploads: ', pathToUploads);
@@ -36,6 +36,11 @@ export const markdownImporterEndpoint = async (request: Request, response: Respo
         const filePath: string = uploadedStatementFile.path;
         const content: string = fs.readFileSync(filePath).toString();
 
+        const md = markdownit()
+        const result = md.render(content);
+        console.log('Markdown content rendered: ', result);
+
+
       }
 
       const responseData = {
@@ -45,41 +50,4 @@ export const markdownImporterEndpoint = async (request: Request, response: Respo
     }
   });
 
-  // try {
-  //   console.log(request.body);
-
-  //   const files: FileToImport[] = request.body.files;
-  //   const fileNames: string[] = files.map((file) => file.name);
-
-  //   console.log('files:', files);
-  //   console.log('fileNames:', fileNames);
-
-  //   // for (const fileName of fileNames) {
-  //   //   let updatedFileName: string = '';
-  //   //   const filePath = path.join(baseDirectory, fileName);
-  //   //   const fileExtension = path.extname(filePath);
-  //   //   if (fileExtension.toLowerCase() === '.heic' || fileExtension.toLowerCase() === '.heif') {
-  //   //     const inputFilePath = filePath;
-  //   //     const dirname = path.dirname(inputFilePath); // Extracts the directory path
-  //   //     const newFilename = path.basename(inputFilePath, fileExtension) + ".jpg"; // Replaces .heic with .jpg
-  //   //     const outputFilePath = path.join(dirname, newFilename); // Combines directory with new filename
-  //   //     console.log('convertFilesToJpeg:', inputFilePath, outputFilePath);
-
-  //   //     const fileEntry = processingStatuses[importId].files.find((f) => f.filename === fileName);
-
-
-  //   //   } else {
-  //   //     updatedFileName = fileName;
-  //   //   }
-
-  //   //   const mediaItem: MediaItem = await buildLocalStorageMediaItem(baseDirectory, albumId, updatedFileName, googleAlbumName, googleAlbumId);
-  //   //   await addMediaItemsFromLocalStorage([mediaItem]);
-
-  //   // }
-
-  //   response.json({});
-  // } catch (error) {
-  //   console.error('Error in importPhotosEndpoint:', error);
-  //   response.status(500).json(error);
-  // }
 }
