534f79317ff0224e1212ffa96cf71fa12fc71e93
Ted Shaffer <shaffer.family@gmail.com>
2025-06-21 05:20:23 -0700

    Remove build artifacts now covered by .gitignore

--- backend/dist/config/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TMP = void 0;
-exports.TMP = "tmp";
-//# sourceMappingURL=constants.js.map
--- backend/dist/routes/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createRoutes = void 0;
-const controllers_1 = require("../controllers");
-const createRoutes = (app) => {
-    app.get('/api/v1/version', controllers_1.getVersion);
-};
-exports.createRoutes = createRoutes;
-//# sourceMappingURL=routes.js.map
