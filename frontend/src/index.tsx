632faa00fd97ff0f15ea43dbb69c4567fa94fe9b
Ted Shaffer <shaffer.family@gmail.com>
2025-06-23 07:02:15 -0700

    more tweaks / cleanup

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,36 +1,28 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import thunkMiddleware from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import AppShell from './components/AppShell';
 import { initializeDiagnostics, loadEnvConfig } from './utilities';
 
-import { store } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
 
-{/* <Provider store={store}>
-  <AppShell />
-</Provider> */}
+import { store } from './redux/store';
 
 window.React = React;
 
 initializeDiagnostics();
 
-const container = document.getElementById('content');
-const root = createRoot(container!);
-
 loadEnvConfig().then(() => {
   console.log('index.tsx, serverUrl:', (window as any).__ENV__?.BACKEND_URL);
-  root.render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<AppShell />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
-  );
+  createRoot(document.getElementById('content')!).render(
+    <StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <AppShell />
+        </BrowserRouter>
+      </Provider>
+    </StrictMode>
+  )
 });
+
