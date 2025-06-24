455a174d63987af99ba7c02efe4e82b43f05488c
Ted Shaffer <shaffer.family@gmail.com>
2025-06-24 05:19:50 -0700

    Add endpoint to getProjects..

---
+++ backend/src/controllers/projects.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+
+import { ParsedMarkdown } from "../types";
+import { ProjectModel } from "../models";
+
+export const getProjects = async (request: Request, response: Response, next: any) => {
+  try {
+    const projects = await ProjectModel.find().lean(); // retrieve all from DB
+    const parsedMarkdown: ParsedMarkdown = { projects };
+    response.json(parsedMarkdown);
+  } catch (error) {
+    console.error('Error fetching projects:', error);
+    response.status(500).json({ error: 'Failed to fetch projects' });
+  }
+}
--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -3,9 +3,12 @@ import {
   getVersion,
   markdownImporterEndpoint,
 } from '../controllers';
+import { getProjects } from '../controllers/projects';
 
 export const createRoutes = (app: express.Application) => {
   app.get('/api/v1/version', getVersion);
+
+  app.get('/api/v1/projects', getProjects);
 
   app.post('/api/v1/importMarkdown', markdownImporterEndpoint);
 
